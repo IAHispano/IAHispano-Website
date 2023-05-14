70c979479e7cd5179a8441c7a9bade07cb290ea5
IA Hispano <133521603+IAHispano0@users.noreply.github.com>
2023-05-14 22:59:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-const express = require('express');
 const ytdl = require('ytdl-core');
-const fs = require('fs');
-const path = require('path');
 
-const app = express();
-
-app.use(express.static('public'));
-
-app.get('/download', async (req, res) => {
-  const videoURL = req.query.url;
+module.exports = async (req, res) => {
+  const { url } = req.query;
 
   try {
-    const info = await ytdl.getInfo(videoURL);
+    const info = await ytdl.getInfo(url);
     const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
 
-    res.header('Content-Disposition', 'attachment; filename="audio.wav"');
-    ytdl(videoURL, { format: format }).pipe(res);
+    res.setHeader('Content-Disposition', 'attachment; filename="audio.wav"');
+    ytdl(url, { format: format }).pipe(res);
   } catch (error) {
     console.error('Error al obtener información del video:', error);
     res.status(500).send('Error al obtener información del video');
   }
-});
+};
 
-app.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000');
-});
