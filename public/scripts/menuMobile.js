9e346e72bcb67eaf60aa32968f7283976462c1ef
Alejandro Montoya <alejocapuchinom@gmail.com>
2023-05-16 18:49:59 -0600

    Menu Mobile

---
+++ public/scripts/menuMobile.js
@@ -0,0 +1,10 @@
+//This code is so that when making the screen smaller the manu snack appears and when clicking on the menu the menu is displayed
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const mobileMenu = document.querySelector('.mobile-menu');
+  
+    menuToggle.addEventListener('click', function () {
+      this.classList.toggle('active');
+      mobileMenu.classList.toggle('active');
+    });
+  });
