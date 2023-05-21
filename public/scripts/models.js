e44e07a530272e0f87554e44aeb467857ea30081
blaise <133521603+IAHispano0@users.noreply.github.com>
2023-05-21 16:38:33 +0000

    [added] some changes

--- public/scripts/models.js
+++ public/scripts/models.js
@@ -1,60 +1,59 @@
 // Deshabilitar la selección y el arrastre de imágenes en la página
-document.addEventListener('DOMContentLoaded', function() {
-    disableSelectionAndDrag(document.body);
-  });
-  
-  function disableSelectionAndDrag(element) {
-    element.setAttribute('unselectable', 'on');
-    element.style.userSelect = 'none';
-    element.style.webkitUserSelect = 'none';
-    element.style.msUserSelect = 'none';
-    element.style.mozUserSelect = 'none';
-  
-    var images = element.querySelectorAll('img');
-    for (var i = 0; i < images.length; i++) {
-      images[i].setAttribute('draggable', 'false');
-    }
-  
-    var children = element.children;
-    for (var i = 0; i < children.length; i++) {
-      disableSelectionAndDrag(children[i]);
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  disableSelectionAndDrag(document.body);
+});
+
+function disableSelectionAndDrag(element) {
+  element.setAttribute("unselectable", "on");
+  element.style.userSelect = "none";
+  element.style.webkitUserSelect = "none";
+  element.style.msUserSelect = "none";
+  element.style.mozUserSelect = "none";
+
+  var images = element.querySelectorAll("img");
+  for (var i = 0; i < images.length; i++) {
+    images[i].setAttribute("draggable", "false");
   }
-  
 
-  // Etiquetas (Tags)
-  function filterImages(tag) {
-    var tags = document.getElementsByClassName("tag");
-    for (var i = 0; i < tags.length; i++) {
-      tags[i].classList.remove("selected");
-    }
+  var children = element.children;
+  for (var i = 0; i < children.length; i++) {
+    disableSelectionAndDrag(children[i]);
+  }
+}
 
-    var selectedTag = document.getElementById("tag-" + tag);
-    if (selectedTag) {
-      selectedTag.classList.add("selected");
-    }
+// Etiquetas (Tags)
+function filterImages(tag) {
+  var tags = document.getElementsByClassName("tag");
+  for (var i = 0; i < tags.length; i++) {
+    tags[i].classList.remove("selected");
+  }
 
-    var articles = document.querySelectorAll(".model-gallery article");
+  var selectedTag = document.getElementById("tag-" + tag);
+  if (selectedTag) {
+    selectedTag.classList.add("selected");
+  }
 
-    articles.forEach(function (article) {
-      var labels = article.querySelectorAll(".label");
-      var image = article.querySelector("img");
+  var articles = document.querySelectorAll(".model-gallery article");
 
-      var hasTag = false;
+  articles.forEach(function (article) {
+    var labels = article.querySelectorAll(".label");
+    var image = article.querySelector("img");
 
-      labels.forEach(function (label) {
-        if (label.innerText === tag || tag === "") {
-          hasTag = true;
-        }
-      });
+    var hasTag = false;
 
-      if (hasTag) {
-        article.style.display = "block";
-      } else {
-        article.style.display = "none";
+    labels.forEach(function (label) {
+      if (label.innerText === tag || tag === "") {
+        hasTag = true;
       }
     });
-  }
+
+    if (hasTag) {
+      article.style.display = "block";
+    } else {
+      article.style.display = "none";
+    }
+  });
+}
 
 // Audio
 // Working on...
--- public/scripts/videoScrape.js
+++ public/scripts/videoScrape.js
@@ -1,35 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Array de URL de los videos de YouTube
   const videoUrls = [
-    'https://www.youtube.com/watch?v=sD5gGMdUndE',
-    'https://www.youtube.com/watch?v=YkXAFMp6fj0',
-    'https://www.youtube.com/watch?v=ws9CgWTFwg4',
-    'https://www.youtube.com/watch?v=gJcFvGIDeTE',
-    'https://www.youtube.com/watch?v=gJcFvGIDeTE',
-    'https://www.youtube.com/watch?v=gJcFvGIDeTE',
-    'https://www.youtube.com/watch?v=gJcFvGIDeTE',
-    'https://www.youtube.com/watch?v=gJcFvGIDeTE',
-    // Agrega más URL de videos aquí...
+    "https://www.youtube.com/watch?v=bWSG2yi-VJA",
+    "https://www.youtube.com/watch?v=sD5gGMdUndE",
+    "https://www.youtube.com/watch?v=YkXAFMp6fj0",
+    "https://www.youtube.com/watch?v=ws9CgWTFwg4",
+    "https://www.youtube.com/watch?v=OyC6A0AsRRE",
+    "https://www.youtube.com/watch?v=uyb2iDjR970",
+    "https://www.youtube.com/watch?v=7UvshC1Qdps",
+    "https://www.youtube.com/watch?v=eOBOtYbt_3w",
+    "https://www.youtube.com/watch?v=gJcFvGIDeTE",
+    "https://www.youtube.com/watch?v=IKuhZUlWUnc",
   ];
 
   function loadVideos() {
-    const container = document.getElementById('video-container');
+    const container = document.getElementById("video-container");
 
-    videoUrls.forEach(videoUrl => {
+    videoUrls.forEach((videoUrl) => {
       const videoId = getVideoIdFromUrl(videoUrl);
 
       // Crear el cuadro del video
-      const videoWrapper = document.createElement('div');
-      videoWrapper.className = 'video-wrapper';
+      const videoWrapper = document.createElement("div");
+      videoWrapper.className = "video-wrapper";
 
       // Crear la miniatura del video
-      const thumbnailLink = document.createElement('a');
+      const thumbnailLink = document.createElement("a");
       thumbnailLink.href = videoUrl;
-      thumbnailLink.target = '_blank';
+      thumbnailLink.target = "_blank";
 
-      const thumbnailImg = document.createElement('img');
+      const thumbnailImg = document.createElement("img");
       thumbnailImg.src = `https://img.youtube.com/vi/${videoId}/mqdefault.jpg`;
-      thumbnailImg.alt = 'Video Thumbnail';
+      thumbnailImg.alt = "Video Thumbnail";
 
       thumbnailLink.appendChild(thumbnailImg);
       videoWrapper.appendChild(thumbnailLink);
@@ -38,11 +39,11 @@ document.addEventListener('DOMContentLoaded', function() {
       const videoTitle = getVideoTitle(videoUrl);
 
       // Crear el título del video
-      const titleLink = document.createElement('a');
+      const titleLink = document.createElement("a");
       titleLink.href = videoUrl;
-      titleLink.target = '_blank';
+      titleLink.target = "_blank";
       titleLink.textContent = videoTitle;
-      titleLink.className = 'video-title'; // Agrega la clase CSS para dar estilo al título
+      titleLink.className = "video-title"; // Agrega la clase CSS para dar estilo al título
 
       videoWrapper.appendChild(titleLink);
 
@@ -50,9 +51,9 @@ document.addEventListener('DOMContentLoaded', function() {
       const videoDescription = getVideoDescription(videoUrl);
 
       // Crear la descripción del video
-      const descriptionPara = document.createElement('p');
+      const descriptionPara = document.createElement("p");
       descriptionPara.textContent = videoDescription;
-      descriptionPara.className = 'video-description'; // Agrega la clase CSS para dar estilo a la descripción
+      descriptionPara.className = "video-description"; // Agrega la clase CSS para dar estilo a la descripción
 
       videoWrapper.appendChild(descriptionPara);
 
@@ -63,33 +64,44 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function getVideoIdFromUrl(videoUrl) {
     const urlParams = new URLSearchParams(new URL(videoUrl).search);
-    return urlParams.get('v');
+    return urlParams.get("v");
   }
 
   function getVideoTitle(videoUrl) {
     // Aquí puedes introducir manualmente los títulos de los videos correspondientes
     const videoTitles = {
-      'https://www.youtube.com/watch?v=sD5gGMdUndE': 'Título del video 1',
-      'https://www.youtube.com/watch?v=YkXAFMp6fj0': 'Título del video 2',
-      'https://www.youtube.com/watch?v=ws9CgWTFwg4': 'Título del video 3',
-      'https://www.youtube.com/watch?v=gJcFvGIDeTE': 'Título del video 4',
-      // Agrega más títulos aquí...
+      "https://www.youtube.com/watch?v=bWSG2yi-VJA": "COMO HACER UN DATASET",
+      "https://www.youtube.com/watch?v=sD5gGMdUndE": "CREAR CANCIONES CON IA",
+      "https://www.youtube.com/watch?v=YkXAFMp6fj0": "CREAR MODELOS IA",
+      "https://www.youtube.com/watch?v=ws9CgWTFwg4": "TUTORIAL RVC",
+      "https://www.youtube.com/watch?v=OyC6A0AsRRE": "SAIKO - UN VERANO SIN TI",
+      "https://www.youtube.com/watch?v=uyb2iDjR970": "ANTES DE PERDERTE",
+      "https://www.youtube.com/watch?v=7UvshC1Qdps": "POLARIS",
+      "https://www.youtube.com/watch?v=eOBOtYbt_3w": `SHE DON'T GIVE A FO`,
+      "https://www.youtube.com/watch?v=gJcFvGIDeTE": "MEMORIAS",
+      "https://www.youtube.com/watch?v=IKuhZUlWUnc": "DÁKITI",
     };
 
-    return videoTitles[videoUrl] || 'Video no disponible';
+    return videoTitles[videoUrl] || "Video no disponible";
   }
 
   function getVideoDescription(videoUrl) {
     // Aquí puedes introducir manualmente las descripciones de los videos correspondientes
     const videoDescriptions = {
-      'https://www.youtube.com/watch?v=sD5gGMdUndE': 'Descripción del video 1',
-      'https://www.youtube.com/watch?v=YkXAFMp6fj0': 'Descripción del video 2',
-      'https://www.youtube.com/watch?v=ws9CgWTFwg4': 'Descripción del video 3',
-      'https://www.youtube.com/watch?v=gJcFvGIDeTE': 'Descripción del video 4',
+      "https://www.youtube.com/watch?v=bWSG2yi-VJA": "COMO HACER UN DATASET",
+      "https://www.youtube.com/watch?v=sD5gGMdUndE": "CREAR CANCIONES CON IA",
+      "https://www.youtube.com/watch?v=YkXAFMp6fj0": "CREAR MODELOS IA",
+      "https://www.youtube.com/watch?v=ws9CgWTFwg4": "TUTORIAL RVC",
+      "https://www.youtube.com/watch?v=OyC6A0AsRRE": "SAIKO - UN VERANO SIN TI",
+      "https://www.youtube.com/watch?v=uyb2iDjR970": "ANTES DE PERDERTE",
+      "https://www.youtube.com/watch?v=7UvshC1Qdps": "POLARIS",
+      "https://www.youtube.com/watch?v=eOBOtYbt_3w": `SHE DON'T GIVE A FO`,
+      "https://www.youtube.com/watch?v=gJcFvGIDeTE": "MEMORIAS",
+      "https://www.youtube.com/watch?v=IKuhZUlWUnc": "DÁKITI",
       // Agrega más descripciones aquí...
     };
 
-    return videoDescriptions[videoUrl] || 'Descripción no disponible';
+    return videoDescriptions[videoUrl] || "Descripción no disponible";
   }
 
   loadVideos();
