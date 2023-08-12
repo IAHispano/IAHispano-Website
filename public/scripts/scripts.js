1d3d86a3652c1bb9f15f180102452f668d765d6a
bygimenez <davidalbal123@gmail.com>
2023-08-12 18:21:31 +0200

    some changes

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,8 +1,4 @@
 function showFaq(element) {
-  // let open = element.getElementsByClassName("faq-answer")[0].classList.contains("faq-show");
-
-  // if(open){
-  //     element.getElementsByClassName("faq-answer")[0]
-  // }
-  element.getElementsByClassName("faq-answer")[0].classList.toggle("faq-show");
+  const answer = element.querySelector(".faq-answer");
+  answer.classList.toggle("faq-show");
 }
